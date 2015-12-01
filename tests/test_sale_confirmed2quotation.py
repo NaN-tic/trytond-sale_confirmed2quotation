311e0284af5773e2f471f431e3db9f0845c82b23
resteve <resteve@zikzakmedia.com>
2015-12-01 11:18:05 +0100

    Tests

--- tests/test_sale_confirmed2quotation.py
+++ tests/test_sale_confirmed2quotation.py
@@ -1,29 +1,18 @@
-#!/usr/bin/env python
-# This file is part of sale_confirmed2quotation module for Tryton.
+# This file is part of the sale_confirmed2quotation module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends, test_view
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleConfirmed2QuotationTestCase(unittest.TestCase):
-    'Test Sale Confirmed to Quotation module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_confirmed2quotation')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_confirmed2quotation')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class SaleConfirmed2quotationTestCase(ModuleTestCase):
+    'Test Sale Confirmed2quotation module'
+    module = 'sale_confirmed2quotation'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        SaleConfirmed2QuotationTestCase))
+        SaleConfirmed2quotationTestCase))
     return suite
